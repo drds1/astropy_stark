c5cff5eda8fe0aaa81da9a88db401a8aa20a4987
dstarkey23 <david.starkey@vortexa.com>
2021-02-10 18:57:17 +0000

    remove outdated glob3 module from requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 #pip install .
 #python setup.py sdist
-#twine upload dist/astropy_stark-1.1.9.tar.gz
+#twine upload dist/astropy_stark-1.1.10.tar.gz
 setup(name='astropy_stark',
-      version='1.1.9',
+      version='1.1.10',
       description='random-custom astronomy functions including making fake accretion disk light curves',
       url='https://github.com/dstarkey23/pycecream',
       author='dstarkey23',
@@ -18,7 +18,6 @@ setup(name='astropy_stark',
       'pandas',
       'numpy',
       'matplotlib',
-      'glob3',
       ],
       zip_safe=False)
 
